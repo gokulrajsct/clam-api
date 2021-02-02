e965211547ace2f6ab31e8302a8db77bdbba94ee
gokulrajsct <gokulrajsct@gmail.com>
2021-02-02 16:07:23 +1300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -m
 
-host=${CLAMD_HOST:-192.168.50.72}
+host=${CLAMD_HOST:-0.0.0.0}
 port=${CLAMD_PORT:-3310}
 filesize=${MAXSIZE:-10240MB}
 timeout=${TIMEOUT:-10000}
@@ -10,4 +10,4 @@ echo "using clamd server: $host:$port"
 
 # start in background
 #java -jar /var/clamav-rest/clamav-1.0.2.jar --clamd.host=$host --clamd.port=$port
-java -jar /var/clamav-rest/clamav-1.0.2.jar --clamd.host=$host --clamd.port=$port --clamd.maxfilesize=$filesize --clamd.maxrequestsize=$filesize --clamd.timeout=$timeout
+java -jar /var/clamav-rest/clamav-rest-1.0.2.jar --clamd.host=$host --clamd.port=$port --clamd.maxfilesize=$filesize --clamd.maxrequestsize=$filesize --clamd.timeout=$timeout
